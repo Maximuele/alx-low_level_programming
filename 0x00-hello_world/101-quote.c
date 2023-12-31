3b5c138a0b2a708a1b10de4101c39d1017797330
Maximuele <markokots@gmail.com>
2023-12-31 21:48:53 +0300

    worked?

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 return (1);
 }
