5cbdbc6f62ff495d8a842a3740870b065e2a6d8f
Maximuele <markokots@gmail.com>
2024-01-01 05:34:50 +0300

    upper and lowercase alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *print lower case aplphabet
+ *using putchar
+ *Return: Always 0 (sucess)
+ */
+int main(void)
+{
+char alpha;
+char cap;
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+for (cap = 'A'; cap <= 'Z'; cap++)
+{
+putchar(cap);
+}
+return (0);
+
+}
