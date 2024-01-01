4daad26ed5e1c10af0a05a67a93280164957e1da
Maximuele <markokots@gmail.com>
2024-01-01 03:00:56 +0300

    working in negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
+
 /**
  *main - Entry point
  *Positive or negative interger
@@ -7,20 +9,19 @@
  */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n % 2 > 0)
-	{
-	printf("%d is greater than 0: is positive\n", n);
-	}
-	if (n % 2 == 0)
-	{
-	printf("%d is 0: is zero\n", n);
-	}
-	else
-	printf("%d is less than 0: is negative\n", n);
-	return(0);
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    if (n > 0)
+    {
+        printf("%d is greater than 0: is positive\n", n);
+    } 
+    if (n == 0)
+    {
+        printf("%d is 0: is zero\n", n);
+    }
+    else
+        printf("%d is less than 0: is negative\n", n);
+    return(0);
 }
