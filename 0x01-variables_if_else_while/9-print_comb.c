7def01a10069e560f3557f1640f749afecc8ce41
Maximuele <markokots@gmail.com>
2024-01-01 21:30:29 +0300

    working well

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,14 @@
 int main(void)
 {
 int i;
-int j;
 
 for (i = 48; i < 58; i++)
 {
-for (j = 48; j < 58; j++)
+putchar(i);
+if (i < 57)
 {
-putchar(i + j);
+putchar(44);
+putchar(32);
 }
 }
 putchar('\n');
