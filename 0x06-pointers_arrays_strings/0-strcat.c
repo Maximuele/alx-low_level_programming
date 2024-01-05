429a85501e8b981f10120855234313564b620aa7
Maximuele <markokots@gmail.com>
2024-01-05 04:44:32 +0300

    hello

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+char *_strcat(char *dest, char *src)
+{
+int count1 = 0, count2 = 0;
+
+while (*(dest + count1) != '\0')
+{
+	count1++;
+}
+
+while (count2 >= 0)
+{
+*(dest = count1) = *(src + count2);
+
+if (*(src + count2) == '\0')
+	break;
+count1++;
+count2++;
+}
+return (dest);
+
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+ALX PROJECT
+
+0x06-pointers arrays and strings
+
+Tasks
+
