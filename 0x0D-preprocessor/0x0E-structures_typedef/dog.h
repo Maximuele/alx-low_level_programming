697c6fd6b6e159dba4c770189b3ec8c4afe2f4d0
Maximuele <markokots@gmail.com>
2024-01-23 02:05:58 +0300

    structures

--- 0x0D-preprocessor/0x0E-structures_typedef/dog.h
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -1,20 +1,24 @@
-#ifndef _DOG_H
-#define _DOG_H
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
- *main- entry point
- *@name - first variable pointer
- *@age - second variable
- *@owner - third variable pointer
- * struct declaration
+ * struct dog - struture of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}; /* do.h */
 
+}
+dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
