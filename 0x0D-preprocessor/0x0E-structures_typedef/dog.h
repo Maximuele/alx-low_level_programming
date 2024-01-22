e1baad0724decf9ff623b4e2fed9d2bcdd977d28
Maximuele <markokots@gmail.com>
2024-01-23 01:31:26 +0300

    attempt 1

---
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+#include <stdio.h>
+
+/**
+ *main- entry point
+ *@name - first variable pointer
+ *@age - second variable
+ *@owner - third variable pointer
+ * struct declaration
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}; /* do.h */
+
+#endif
